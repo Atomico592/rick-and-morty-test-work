4568767a09fe27c0e55cea7ecedf9ea75be35063
Atomico592 <atomico592@gmail.com>
2024-06-11 18:30:12 +0600

    Added pagination logic

--- src/App.vue
+++ src/App.vue
@@ -8,49 +8,69 @@
   </div>
   </div>
   <div v-if="error">{{error}}</div>
+
+  <Pagination :totalPage="totalPage" :currentPage="currentPage" @page-changed="changePage" />
+
   <div v-if="charactersData.length">
     <Card v-for="character in charactersData" :key="charactersData.id" :character="character"  :characters-data="character"/>
   </div>
 </div>
 </template>
 <script>
-import { ref, watch, onMounted } from 'vue';
+import { ref, watch } from 'vue';
 import axios from 'axios';
 import Card from "@/components/Card.vue";
+import Pagination from "@/components/Pagination.vue";
 import preloader from "./assets/preloader.gif";
 
 export default {
- components: {Card},
+ components: {Card, Pagination},
 
 setup() {
   const charactersData = ref([]);
   const loading = ref(false);
   const error = ref(null);
+  const currentPage = ref(1);
+  const totalPage = ref ({
+    name: '',
+    status: ''
+  });
 
 const fetchCharactertsList = async () => {
     loading.value = true;
     error.value = null;
     try {
-      const { data } = await axios('https://rickandmortyapi.com/api/character')
+      const { data } = await axios('https://rickandmortyapi.com/api/character', {
+        params : {
+          page: currentPage.value,
+        }
+      });
 
       if(data) {
         charactersData.value = data.results;
       }
       loading.value = false;
-
+      totalPage.value = data.info.pages;
     } catch (e) {
       loading.value = false;
       error.value = "An error occurred while loading data";
     }
 };
 
-watch(fetchCharactertsList, {immediate: true})
+const changePage = (page) => {
+  currentPage.value = page;
+};
+
+watch([currentPage] ,fetchCharactertsList, {immediate: true})
 
   return {
    charactersData,
     loading,
     error,
-    preloader
+    preloader,
+    currentPage,
+    totalPage,
+    changePage
   };
  }
 };
--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,10 +1,31 @@
-<script setup>
+<template>
+<div v-if="totalPage > 1">
+      <button @click="changePage(page)" v-for="page in totalPage">{{ page }}</button>
+</div>
+</template>
+
+<script>
+
+  export default {
+    props : {
+      totalPage: {
+        type: Number,
+        required: true
+      },
+      currentPage : {
+        type: Number,
+        required: true
+      }
+     },
+    emits: ['page-changed'],
+    methods : {
+      changePage(page) {
+        this.$emit("page-changed", page);
+      }
+    }
+  };
 
 </script>
-
-<template>
-
-</template>
 
 <style scoped>
 
